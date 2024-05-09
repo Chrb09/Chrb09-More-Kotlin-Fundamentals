6ee8572aa4fd99f2ab20ccf9222aac5f5d6a278a
Chrb09 <carloshrbarile@gmail.com>
2024-05-09 20:32:12 -0300

    atividade2

--- script.kt
+++ script.kt
@@ -1,15 +1,18 @@
 fun main() {
-    val event1 = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", "Evening", 15)
+    val event1 = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", Daypart.EVENING, 15)
     println(event1.toString())
 }
 
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: String,
+    val daypart: Daypart,
     val duration: Int,
 )
 
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING,
+}
+
 /* Console:
-Event(title=Study Kotlin, description=Commit to studying Kotlin at least 15 minutes per day., daypart=Evening, duration=15)
- */
+Event(title=Study Kotlin, description=Commit to studying Kotlin at least 15 minutes per day., daypart=EVENING, duration=15)*/
